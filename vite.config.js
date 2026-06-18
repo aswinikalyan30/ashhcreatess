fbe21d66de6f70aebdb188643546e50e97dd3ce7
Aswini Kalyan <ashwinikalyan30@gmail.com>
2026-06-18 16:25:14 +0530

    optimise image

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteImageOptimizer from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      mozjpeg: {
+        quality: 85,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+      },
+      webp: {
+        quality: 85,
+      },
+    }),
+  ],
 })
